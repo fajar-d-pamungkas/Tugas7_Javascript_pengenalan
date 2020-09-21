262acd373ce886eb47a40ed5ad44f2fdfcc757a9
fajar-d-pamungkas <71364634+fajar-d-pamungkas@users.noreply.github.com>
2020-09-21 08:11:09 +0700

    Add files via upload

---
+++ tugas7.js
@@ -0,0 +1,13 @@
+console.log("Perulangan Biasa :")
+
+var dataTB = [165,166,167,168,169,170,171,172,173,174]
+
+for (i = 0; i<10 ; i++){
+	console.log(dataTB[i])
+}
+
+console.log("Perulangan For of :")
+
+for (let ulang of dataTB){
+	console.log(ulang)
+}
